2a9f3e40495dbf8cb2dda5cbd2d7a8379f4df55b
Benito <jorgebenitoluislopez@gmail.com>
2026-07-20 15:55:54 -0600

    V 20Jul

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ from sqlalchemy.orm import Session
 import crud, models, schemas
 from database import SessionLocal, engine
 
-# Crear tablas automáticamente
+
 models.Base.metadata.create_all(bind=engine)
 
 app = FastAPI()
@@ -16,12 +16,11 @@ def get_db():
     finally:
         db.close()
 
-# GET ALL
+
 @app.get("/productos")
 def read_productos(db: Session = Depends(get_db)):
     return crud.get_productos(db)
 
-# GET BY ID
 @app.get("/productos/{id}")
 def read_producto(id: int, db: Session = Depends(get_db)):
     producto = crud.get_producto(db, id)
@@ -29,12 +28,12 @@ def read_producto(id: int, db: Session = Depends(get_db)):
         raise HTTPException(status_code=404, detail="Producto no encontrado")
     return producto
 
-# CREATE
+
 @app.post("/productos")
 def create_producto(producto: schemas.ProductoCreate, db: Session = Depends(get_db)):
     return crud.create_producto(db, producto)
 
-# UPDATE
+
 @app.put("/productos/{id}")
 def update_producto(id: int, producto: schemas.ProductoUpdate, db: Session = Depends(get_db)):
     updated = crud.update_producto(db, id, producto)
@@ -42,7 +41,6 @@ def update_producto(id: int, producto: schemas.ProductoUpdate, db: Session = Dep
         raise HTTPException(status_code=404, detail="Producto no encontrado")
     return updated
 
-# DELETE
 @app.delete("/productos/{id}")
 def delete_producto(id: int, db: Session = Depends(get_db)):
     deleted = crud.delete_producto(db, id)
